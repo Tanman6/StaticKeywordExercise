e1d9a83b35161bf63e231899c38b5f4fd2694cf9
Tanman6 <tanmansyx@protonmail.com>
2022-02-23 17:30:09 -0600

    initial commit

---
+++ StaticKeywordExercise/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace StaticKeywordExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TempConverter.FahrenheitToCelsius(80);
+            TempConverter.CelciusToFahrenheit(22);
+        }
+    }
+}
---
+++ StaticKeywordExercise/TempConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace StaticKeywordExercise
+{
+    public class TempConverter
+    {
+        public static double FahrenheitToCelsius(double number)
+        {
+            double F = (number - 32) * .5555;
+            Console.WriteLine($"{F}");
+            return F;
+        }
+
+        public static double CelciusToFahrenheit(double num)
+        {
+            double C = num * 1.8 + 32;
+            Console.WriteLine($"{C}");
+            return C;
+        }
+
+        public TempConverter()
+        {
+        }
+    }
+}
